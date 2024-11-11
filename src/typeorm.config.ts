e780c588b045890cfbf21cc4360dcb955b78a3f5
elie attieh <elieatteah00.mme@gmail.com>
2024-11-11 16:10:25 +0300

    Refactor: move db creds to the .env file

--- src/typeorm.config.ts
+++ src/typeorm.config.ts
@@ -1,13 +1,15 @@
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
+import { config } from 'dotenv';
+config({ path: '.env.dev' });
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  database: 'postgres',
-  port: 5450,
-  username: 'postgres',
-  password: 'postgres',
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   synchronize: true,
   logging: false,
   entities: [__dirname + '/entities/*{.js,.ts}'],
